76b5c1997f8140298d1461c7e24e47298692f4d9
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2022-11-05 10:05:36 -0700

    docs: delete comments re non-existent directory style: prettify code

--- src/FungibleV3LP.sol
+++ src/FungibleV3LP.sol
@@ -6,46 +6,60 @@ import "./UniswapV3position.sol";
 import "dependencies/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
-//Note: could not import from original location, so declaring here for now.
-//import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";a
 interface IERC20Metadata is IERC20 {
     function name() external view returns (string memory);
+
     function symbol() external view returns (string memory);
+
     function decimals() external view returns (uint8);
 }
 
-
-//debugging
-import "hardhat/console.sol";
-
-
 contract FungibleV3LP is UniswapV3position, ERC20 {
-
     //pool variables
     address tokenA;
     address tokenB;
     uint24 fee;
 
     constructor(
-        INonfungiblePositionManager _nonfungiblePositionManager,     
+        INonfungiblePositionManager _nonfungiblePositionManager,
         address _tokenA,
         address _tokenB,
-        uint24 _fee) 
+        uint24 _fee
+    )
         UniswapV3position(_nonfungiblePositionManager)
-        ERC20(tName(_tokenA,_tokenB),tSymbol(_tokenA,_tokenB)) 
-        {
+        ERC20(tName(_tokenA, _tokenB), tSymbol(_tokenA, _tokenB))
+    {
         tokenA = _tokenA;
         tokenB = _tokenB;
         fee = _fee;
     }
 
     function tName(address a, address b) private view returns (string memory) {
-        return string(abi.encodePacked(IERC20Metadata(a).name(),IERC20Metadata(b).name(),string('v3LP')));
+        return
+            string(
+                abi.encodePacked(
+                    IERC20Metadata(a).name(),
+                    IERC20Metadata(b).name(),
+                    string("v3LP")
+                )
+            );
     }
-    
-    function tSymbol(address a, address b) private view returns (string memory) {
-        return string(abi.encodePacked(IERC20Metadata(a).symbol(),IERC20Metadata(b).symbol(),string('v3LP')));
+
+    function tSymbol(address a, address b)
+        private
+        view
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    IERC20Metadata(a).symbol(),
+                    IERC20Metadata(b).symbol(),
+                    string("v3LP")
+                )
+            );
     }
 
     function addLiquidity(
@@ -55,14 +69,20 @@ contract FungibleV3LP is UniswapV3position, ERC20 {
         uint amountBMin,
         address to,
         uint deadline
-    ) external   returns (uint amountA, uint amountB, uint liquidity) {
-        
+    )
+        external
+        returns (
+            uint amountA,
+            uint amountB,
+            uint liquidity
+        )
+    {
         //calculate using oracles right tickLower + tickHigher to use
         int24 tickLower = 0;
         int24 tickHigher = 0;
-        
+
         //burn current NFT fully
-        //update liquidity amount to be minted 
+        //update liquidity amount to be minted
 
         uint256 tokenId;
         (tokenId, liquidity, amountA, amountB) = mintNewPosition(
@@ -76,30 +96,21 @@ contract FungibleV3LP is UniswapV3position, ERC20 {
             amountAMin,
             amountBMin
         );
-        
+
         //update tokenID of current active v3 position
 
         //mint ERC20 LP token with 'liquidity' amount in 'to' address wallet
-
     }
 
-     function removeLiquidity(
+    function removeLiquidity(
         uint liquidity,
         uint amountAMin,
         uint amountBMin,
         address to,
         uint deadline
-    ) public   returns (uint amountA, uint amountB) {
-
+    ) public returns (uint amountA, uint amountB) {
         //collect fees
-
         //decrease liquidity by 'liquidity' amount - but keep NFT 'alive'
-
         //send tokens + fees to holder
-
     }
 }
-
-
-
-
