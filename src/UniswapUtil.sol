b5cd8e254580bd62070b96df9d7f72369e02f981
Alan Hampton <alan@amorphous.ai>
2022-11-04 20:35:33 -0700

    public functions for interacting with v3 liquidity

---
+++ src/UniswapUtil.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import "dependencies/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+//import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "dependencies/v3-periphery/contracts/libraries/PoolAddress.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
+//debugging
+import "hardhat/console.sol";
+
+
+//derived from uniswap core 
+library UniswapV3Utils {
+    function createPoolifNecessary(
+        address factory,
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        uint160 _sqrtPriceX96,
+        uint16 observationCardinalityNext
+    ) internal returns (address poolAddress, bool _token0_denominator) {
+        console.log(
+            "create pool block: %s, time: %s",
+            block.number,
+            block.timestamp
+        );
+        console.log("create pool token 0: %s, token 1: %s", _token0, _token1);
+
+        _token0_denominator = (_token1 > _token0);
+        if (!_token0_denominator) (_token0, _token1) = (_token1, _token0);
+
+        poolAddress = IUniswapV3Factory(factory).getPool(
+            _token0,
+            _token1,
+            _fee
+        );
+
+        if (poolAddress == address(0)) {
+            poolAddress = IUniswapV3Factory(factory).createPool(
+                _token0,
+                _token1,
+                _fee
+            );
+            IUniswapV3Pool(poolAddress).initialize(_sqrtPriceX96);
+        } else {
+            (uint160 sqrtPriceX96Existing, , , , , , ) = IUniswapV3Pool(
+                poolAddress
+            ).slot0();
+            if (sqrtPriceX96Existing == 0) {
+                IUniswapV3Pool(poolAddress).initialize(_sqrtPriceX96);
+            }
+        }
+        IUniswapV3Pool(poolAddress).increaseObservationCardinalityNext(
+            observationCardinalityNext
+        );
+        console.log("pool created address: %s", poolAddress);
+    }
+}
---
+++ src/UniswapV3position.sol
@@ -0,0 +1,338 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import "dependencies/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+//import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "dependencies/v3-periphery/contracts/libraries/PoolAddress.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
+//debugging
+import "hardhat/console.sol";
+
+
+// Derived from https://docs.uniswap.org/protocol/guides/providing-liquidity/the-full-contract
+contract UniswapV3position is IERC721Receiver {
+    INonfungiblePositionManager public immutable nonfungiblePositionManager;
+
+    /// @notice Represents the deposit of an NFT
+    struct Deposit {
+        address owner;
+        uint128 liquidity;
+        address token0;
+        address token1;
+    }
+
+    /// @dev deposits[tokenId] => Deposit
+    mapping(uint256 => Deposit) public deposits;
+
+    constructor(INonfungiblePositionManager _nonfungiblePositionManager) {
+        nonfungiblePositionManager = _nonfungiblePositionManager;
+    }
+
+    // Implementing `onERC721Received` so this contract can receive custody of erc721 tokens
+    function onERC721Received(
+        address operator,
+        address,
+        uint256 tokenId,
+        bytes calldata
+    ) external override returns (bytes4) {
+        // get position information
+
+        _createDeposit(operator, tokenId);
+
+        return this.onERC721Received.selector;
+    }
+
+    function _createDeposit(address owner, uint256 tokenId) internal {
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            ,
+            ,
+            ,
+            uint128 liquidity,
+            ,
+            ,
+            ,
+
+        ) = nonfungiblePositionManager.positions(tokenId);
+
+        // set the owner and data for position
+        // operator is msg.sender
+        deposits[tokenId] = Deposit({
+            owner: owner,
+            liquidity: liquidity,
+            token0: token0,
+            token1: token1
+        });
+    }
+
+    /// @notice Calls the mint function defined in periphery, mints the same amount of each token.
+    /// For this example we are providing 1000 DAI and 1000 USDC in liquidity
+    /// @return tokenId The id of the newly minted ERC721
+    /// @return liquidity The amount of liquidity for the position
+    /// @return amount0 The amount of token0
+    /// @return amount1 The amount of token1
+    function mintNewPosition(
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        int24 _tickLower,
+        int24 _tickHigher,
+        uint256 _amount0ToMint,
+        uint256 _amount1ToMint,
+        uint256 _amount0Min,
+        uint256 _amount1Min
+    )
+        external
+        returns (
+            uint256 tokenId,
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        )
+    {
+        console.log("Transfering assets");
+        // transfer tokens to contract
+        if (_amount0ToMint > 0)
+            TransferHelper.safeTransferFrom(
+                _token0,
+                msg.sender,
+                address(this),
+                _amount0ToMint
+            );
+        if (_amount1ToMint > 0)
+            TransferHelper.safeTransferFrom(
+                _token1,
+                msg.sender,
+                address(this),
+                _amount1ToMint
+            );
+
+        console.log("approving assets");
+        // Approve the position manager
+        if (_amount0ToMint > 0)
+            TransferHelper.safeApprove(
+                _token0,
+                address(nonfungiblePositionManager),
+                _amount0ToMint
+            );
+        if (_amount1ToMint > 0)
+            TransferHelper.safeApprove(
+                _token1,
+                address(nonfungiblePositionManager),
+                _amount1ToMint
+            );
+
+        INonfungiblePositionManager.MintParams
+            memory params = INonfungiblePositionManager.MintParams({
+                token0: _token0,
+                token1: _token1,
+                fee: _fee,
+                tickLower: _tickLower,
+                tickUpper: _tickHigher,
+                amount0Desired: _amount0ToMint,
+                amount1Desired: _amount1ToMint,
+                amount0Min: _amount0Min,
+                amount1Min: _amount1Min,
+                recipient: address(this),
+                deadline: block.timestamp
+            });
+
+        console.log("Mint uniswap liquidity position");
+        // Note that the pool must already exist.  Call createPool beforehand just in case
+        (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager
+            .mint(params);
+
+        console.log("Minted uniswap liquidity position");
+
+        // Create a deposit
+        _createDeposit(msg.sender, tokenId);
+
+        // Remove allowance and refund in both assets.
+        if (amount0 < _amount0ToMint) {
+            TransferHelper.safeApprove(
+                _token0,
+                address(nonfungiblePositionManager),
+                0
+            );
+            uint256 refund0 = _amount0ToMint - amount0;
+            TransferHelper.safeTransfer(_token0, msg.sender, refund0);
+        }
+
+        if (amount1 < _amount1ToMint) {
+            TransferHelper.safeApprove(
+                _token1,
+                address(nonfungiblePositionManager),
+                0
+            );
+            uint256 refund1 = _amount1ToMint - amount1;
+            TransferHelper.safeTransfer(_token1, msg.sender, refund1);
+        }
+    }
+
+    /// @notice Collects the fees associated with provided liquidity
+    /// @dev The contract must hold the erc721 token before it can collect fees
+    /// @param tokenId The id of the erc721 token
+    /// @return amount0 The amount of fees collected in token0
+    /// @return amount1 The amount of fees collected in token1
+    function collectAllFees(uint256 tokenId)
+        external
+        returns (uint256 amount0, uint256 amount1)
+    {
+        // Caller must own the ERC721 position, meaning it must be a deposit
+
+        // set amount0Max and amount1Max to uint256.max to collect all fees
+        // alternatively can set recipient to msg.sender and avoid another transaction in `sendToOwner`
+        INonfungiblePositionManager.CollectParams
+            memory params = INonfungiblePositionManager.CollectParams({
+                tokenId: tokenId,
+                recipient: address(this),
+                amount0Max: type(uint128).max,
+                amount1Max: type(uint128).max
+            });
+
+        (amount0, amount1) = nonfungiblePositionManager.collect(params);
+
+        // send collected fee back to owner
+        _sendToOwner(tokenId, amount0, amount1);
+    }
+
+    /// @notice A function that decreases the current liquidity by half. An example to show how to call the `decreaseLiquidity` function defined in periphery.
+    /// @param tokenId The id of the erc721 token
+    /// @return amount0 The amount received back in token0
+    /// @return amount1 The amount returned back in token1
+    function decreaseLiquidityByFactor(uint256 tokenId, uint24 _decreaseFactor)
+        external
+        returns (uint256 amount0, uint256 amount1)
+    {
+        // caller must be the owner of the NFT
+        require(
+            msg.sender == deposits[tokenId].owner,
+            "UniswapV3Utils: Not the position owner"
+        );
+        require(
+            _decreaseFactor < 1000000,
+            "UniswapV3Utils: Decrease factor should be lower than 100%"
+        );
+        // get liquidity data for tokenId
+        uint128 liquidity = deposits[tokenId].liquidity;
+        uint128 NewLiquidity = (liquidity * _decreaseFactor) / 1000000;
+
+        // amount0Min and amount1Min are price slippage checks
+        // if the amount received after burning is not greater than these minimums, transaction will fail
+        INonfungiblePositionManager.DecreaseLiquidityParams
+            memory params = INonfungiblePositionManager
+                .DecreaseLiquidityParams({
+                    tokenId: tokenId,
+                    liquidity: NewLiquidity,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                });
+
+        (amount0, amount1) = nonfungiblePositionManager.decreaseLiquidity(
+            params
+        );
+
+        //send liquidity back to owner
+        _sendToOwner(tokenId, amount0, amount1);
+    }
+
+    /// @notice Increases liquidity in the current range
+    /// @dev Pool must be initialized already to add liquidity
+    /// @param tokenId The id of the erc721 token
+    /// @param amount0 The amount to add of token0
+    /// @param amount1 The amount to add of token1
+    function increaseLiquidityCurrentRange(
+        uint256 tokenId,
+        uint256 amountAdd0,
+        uint256 amountAdd1
+    )
+        external
+        returns (
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        )
+    {
+        TransferHelper.safeTransferFrom(
+            deposits[tokenId].token0,
+            msg.sender,
+            address(this),
+            amountAdd0
+        );
+        TransferHelper.safeTransferFrom(
+            deposits[tokenId].token1,
+            msg.sender,
+            address(this),
+            amountAdd1
+        );
+
+        TransferHelper.safeApprove(
+            deposits[tokenId].token0,
+            address(nonfungiblePositionManager),
+            amountAdd0
+        );
+        TransferHelper.safeApprove(
+            deposits[tokenId].token1,
+            address(nonfungiblePositionManager),
+            amountAdd1
+        );
+
+        INonfungiblePositionManager.IncreaseLiquidityParams
+            memory params = INonfungiblePositionManager
+                .IncreaseLiquidityParams({
+                    tokenId: tokenId,
+                    amount0Desired: amountAdd0,
+                    amount1Desired: amountAdd1,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                });
+
+        (liquidity, amount0, amount1) = nonfungiblePositionManager
+            .increaseLiquidity(params);
+    }
+
+    /// @notice Transfers funds to owner of NFT
+    /// @param tokenId The id of the erc721
+    /// @param amount0 The amount of token0
+    /// @param amount1 The amount of token1
+    function _sendToOwner(
+        uint256 tokenId,
+        uint256 amount0,
+        uint256 amount1
+    ) internal {
+        // get owner of contract
+        address owner = deposits[tokenId].owner;
+
+        address token0 = deposits[tokenId].token0;
+        address token1 = deposits[tokenId].token1;
+        // send collected fees to owner
+        TransferHelper.safeTransfer(token0, owner, amount0);
+        TransferHelper.safeTransfer(token1, owner, amount1);
+    }
+
+    /// @notice Transfers the NFT to the owner
+    /// @param tokenId The id of the erc721
+    function retrieveNFT(uint256 tokenId) external {
+        // must be the owner of the NFT
+        require(msg.sender == deposits[tokenId].owner, "Not the owner");
+        // transfer ownership to original owner
+        nonfungiblePositionManager.safeTransferFrom(
+            address(this),
+            msg.sender,
+            tokenId
+        );
+        //remove information related to tokenId
+        delete deposits[tokenId];
+    }
+}
