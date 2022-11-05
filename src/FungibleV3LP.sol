f60768637710d389e18988e1d8c5283eb670602b
Alan Hampton <alan@amorphous.ai>
2022-11-04 22:42:57 -0700

    feat:create initial addLiquidity and removeLiquidity v2 functions

---
+++ src/FungibleV3LP.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.5;
+pragma abicoder v2;
+
+import "./UniswapV3position.sol";
+import "dependencies/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//debugging
+import "hardhat/console.sol";
+
+
+contract FungibleV3LP is UniswapV3position, ERC20 {
+
+    //pool variables
+    address tokenA;
+    address tokenB;
+    uint24 fee;
+
+    constructor(
+        INonfungiblePositionManager _nonfungiblePositionManager,     
+        address _tokenA,
+        address _tokenB,
+        uint24 _fee) 
+        UniswapV3position(_nonfungiblePositionManager)
+        ERC20('','') 
+        {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+        fee = _fee;
+    }
+
+    function addLiquidity(
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external   returns (uint amountA, uint amountB, uint liquidity) {
+        
+        //calculate using oracles right tickLower + tickHigher to use
+        int24 tickLower = 0;
+        int24 tickHigher = 0;
+        
+        //burn current NFT fully
+        //update liquidity amount to be minted 
+
+        uint256 tokenId;
+        (tokenId, liquidity, amountA, amountB) = mintNewPosition(
+            tokenA,
+            tokenB,
+            fee,
+            tickLower,
+            tickHigher,
+            amountADesired,
+            amountBDesired,
+            amountAMin,
+            amountBMin
+        );
+        
+        //update tokenID of current active v3 position
+
+        //mint ERC20 LP token with 'liquidity' amount in 'to' address wallet
+
+    }
+
+     function removeLiquidity(
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) public   returns (uint amountA, uint amountB) {
+
+        //collect fees
+
+        //decrease liquidity by 'liquidity' amount - but keep NFT 'alive'
+
+        //send tokens + fees to holder
+
+    }
+}
+
+
+
+
